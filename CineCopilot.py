53c8584de143b9371ce8560ba59a882825fe7409
Anmol <behl1anmol@gmail.com>
2023-03-31 13:00:44 +0530

    UI enhancement

--- CineCopilot.py
+++ CineCopilot.py
@@ -26,9 +26,9 @@ st.sidebar.success("Select an app above.")
 
 st.markdown(
     """
-    ## Cine Copilot is an application built using Python and Streamlit which provides movies and songs recommendation to the user. **👈 Select an app from the sidebar** for recommendations of what Streamlit can do!
+    ### Cine Copilot is an application built using Python and Streamlit which provides movies and songs recommendation to the user. **👈 Select an app from the sidebar** for recommendations of what Streamlit can do!
     # Want to learn more?
-    ## - Check out [Github@//CineCopilot](https://github.com/behl1anmol/CineCopilot))
+    ### - Check out [Github@//CineCopilot](https://github.com/behl1anmol/CineCopilot))
 """
 )
 
